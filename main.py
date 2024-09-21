ecc275d6d24f7bee1d2318bad3c55eab02a116a8
anugrahkhirani <anugrahkhirani050521@gmail.com>
2024-09-21 15:15:27 +0700

    latihan coding 21 sep 2024

---
+++ main.py
@@ -0,0 +1,45 @@
+# nilai = int(input("masukkan nilai: "))
+
+# if nilai = nilai + 5
+# print(nilai)
+
+#     print("selamat anda lulus")
+# elif (nilai > 90 and nilai <= 100):
+#     print("selamat anda lulus, dengan nilai yang memuaskan")   
+# else:
+#     print("anda masih belum lulus tes, semangat dan coba lagi")
+
+
+
+# for loop
+# for i in range(1, 10, -2)
+#     print(i)
+#     print("hello world")
+
+#while loop
+# angka = 10
+
+# while (angka > 1):
+#     print(angka)
+#     angka -= 1 #angka = angka - 1
+
+
+
+#[FUNCTION]
+#def
+# def sayHello():
+#     print("Hello icha")
+#     print("selamat pagi")
+
+#     sayHello()
+
+
+#  print("Hello icha")
+#  print("selamat pagi")
+ 
+
+fungsi_kurang4 = lambda param_angka : param_angka - 4
+
+angka = 10
+
+print(fungsi_kurang4(angka))
